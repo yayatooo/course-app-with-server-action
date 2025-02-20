f7ddb56ec3e0fbe039f18a4d077cd3df66be3c83
Rahmat Hidayat <yayatbx@gmail.com>
2025-02-20 11:49:51 +0800

    changes: add getallUser function

--- src/services/user-services.ts
+++ src/services/user-services.ts
@@ -1,29 +1,42 @@
 import { prisma } from "@/utils/prisma";
 import { User } from "@prisma/client";
 
-
 export const userServices = {
-    // make register user service
-    createUser: async (user: Pick<User, "name" | "email" | "password">) => {
-        const newUser = await prisma.user.create({
-            data : {
-                name : user.name,
-                email : user.email,
-                password : user.password
-            }
-        })
+  getAllUsers: async () => {
+    try {
+      const getUsers = await prisma.user.findMany({
+        orderBy: {
+          name: "asc",
+        },
+      });
 
-        return newUser;
-    },
-
-    // make login user service 
-    findUser : async (email: string) => {
-        const findUser = await prisma.user.findUnique({
-            where : {
-                email : email
-            }
-        })
-
-        return findUser;
+      return getUsers;
+    } catch (error) {
+      console.log(error);
     }
-}
+  },
+
+  // make register user service
+  createUser: async (user: Pick<User, "name" | "email" | "password">) => {
+    const newUser = await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    return newUser;
+  },
+
+  // make login user service
+  findUser: async (email: string) => {
+    const findUser = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    return findUser;
+  },
+};
