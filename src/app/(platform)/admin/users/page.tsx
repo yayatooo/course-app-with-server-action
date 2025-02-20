f3bfc0c19b3bdfa978f4a392474bac308c992db6
Rahmat Hidayat <yayatbx@gmail.com>
2025-02-20 11:49:04 +0800

    changes: modify parrent components admin/users

--- src/app/(platform)/admin/users/page.tsx
+++ src/app/(platform)/admin/users/page.tsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { userServices } from "@/services/user-services";
+import React from "react";
+import { BanUserComponents } from "./comp.ban-user";
+import { UnBanUserComponents } from "./comp.unban-user";
 
-export default function page() {
+export default async function page() {
+  const users = await userServices.getAllUsers();
+
   return (
-    <div>Users</div>
-  )
+    <main>
+      <section>User</section>
+      <section>
+        <Table>
+          <TableCaption>A list of current user.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Enroll Courses</TableHead>
+              <TableHead>Role</TableHead>
+              <TableHead className="text-right">Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {users?.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell className="font-medium">{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>0</TableCell>
+                <TableCell>{user.role}</TableCell>
+                <TableCell className="text-right">
+                  {user.onBanned ? (
+                    <UnBanUserComponents userId={user.id} />
+                  ) : (
+                    <BanUserComponents userId={user.id} />
+                  )}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          {/* <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total</TableCell>
+              <TableCell className="text-right">$2,500.00</TableCell>
+            </TableRow>
+          </TableFooter> */}
+        </Table>
+      </section>
+    </main>
+  );
 }
